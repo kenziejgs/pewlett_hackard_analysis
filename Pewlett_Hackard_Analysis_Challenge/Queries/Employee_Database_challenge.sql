1ff479dbb807e2763a2c57535fa9f6cfcbc697bb
Kenzie <106890134+kenziejgs@users.noreply.github.com>
2022-08-28 14:20:00 -0500

    Add files via upload

---
+++ Pewlett_Hackard_Analysis_Challenge/Queries/Employee_Database_challenge.sql
@@ -0,0 +1,59 @@
+--Make table of titles held by retiring employees
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS t
+	ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
+
+INTO nameyourtable
+FROM _______
+WHERE _______
+ORDER BY _____, _____ DESC;
+
+--Remove duplicate employees, keep only most recent job title
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+WHERE (rt.to_date = '9999-01-01')
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+--Create a table counting the number of employees retiring by title
+SELECT COUNT(ut.title), ut.title
+INTO retiring_title_count
+FROM unique_titles AS ut
+GROUP BY (ut.title)
+ORDER BY COUNT (ut.title) DESC;
+
+--Find mentorship eligible employees
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+	INNER JOIN dept_employees AS de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles AS t
+		ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
